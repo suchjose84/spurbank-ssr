f4bf745f77d535f9c200bca76855f99cd6e08792
suchjose84 <suchjose@live.com>
2023-12-09 02:02:58 +0800

    fix fix

--- src/app/blogs/blog01-yuyu-hakusho/blog01-yuyu-hakusho.component.ts
+++ src/app/blogs/blog01-yuyu-hakusho/blog01-yuyu-hakusho.component.ts
@@ -18,19 +18,19 @@ export class Blog01YuyuHakushoComponent implements OnInit {
     this.title.setTitle(this.blogTitle);
 
     // check 2 - Update meata tags
-    this.addTags([
-      { property: 'fb:app_id', content: '887231332962593' },
-      { property: 'og:url', content: 'https://www.spurbank.info/yuyu-hakusho' },
-      { property: 'og:type', content: 'article' },
-      { property: 'og:title', content: 'Yu Yu Hakusho Live-Action Adaptation: A Journey from Manga to Netflix Spectacle' },
-      { property: 'og:description', content: 'Yuyu Hakusho Live-Action coming on Netflix on December 14, 2023' },
-      { property: 'og:image', content: 'http://spurbank-images.s3.ap-southeast-2.amazonaws.com/yuyu-hakusho.png' }
+    this.meta.addTags([
+      { name: 'fb:app_id', content: '887231332962593' },
+      { name: 'og:url', content: 'https://www.spurbank.info/yuyu-hakusho' },
+      { name: 'og:type', content: 'article' },
+      { name: 'og:title', content: 'Yu Yu Hakusho Live-Action Adaptation: A Journey from Manga to Netflix Spectacle' },
+      { name: 'og:description', content: 'Yuyu Hakusho Live-Action coming on Netflix on December 14, 2023' },
+      { name: 'og:image', content: 'http://spurbank-images.s3.ap-southeast-2.amazonaws.com/yuyu-hakusho.png' }
     ]);
 
   }
 
-  private addTags(tags: { property: string, content: string }[]): void {
-    tags.forEach(tag => this.meta.addTag({ property: tag.property, content: tag.content }));
-  }
+  // private addTags(tags: { property: string, content: string }[]): void {
+  //   tags.forEach(tag => this.meta.addTag({ property: tag.property, content: tag.content }));
+  // }
 }
 
