c160d60a12bec54a1c315ee3666b619b3de2b7f6
suchjose84 <suchjose@live.com>
2023-12-09 01:51:25 +0800

    fix fix

--- src/app/blogs/blog01-yuyu-hakusho/blog01-yuyu-hakusho.component.ts
+++ src/app/blogs/blog01-yuyu-hakusho/blog01-yuyu-hakusho.component.ts
@@ -14,25 +14,23 @@ export class Blog01YuyuHakushoComponent implements OnInit {
   constructor(private meta: Meta, private title: Title) { }
 
   ngOnInit(): void {
-    // check 2 - Update tags
+
+    this.title.setTitle(this.blogTitle);
+
+    // check 2 - Update meata tags
     this.addTags([
       { property: 'fb:app_id', content: '887231332962593' },
       { property: 'og:url', content: 'https://www.spurbank.info/yuyu-hakusho' },
       { property: 'og:type', content: 'article' },
       { property: 'og:title', content: 'Yu Yu Hakusho Live-Action Adaptation: A Journey from Manga to Netflix Spectacle' },
-      { property: 'og:description', content: 'Yuyu Hakusho Live-Action coming on Netflix' },
-      { property: 'og:image:secure_url', content: 'https://spurbank-images.s3.ap-southeast-2.amazonaws.com/yuyu-hakusho.png' }
+      { property: 'og:description', content: 'Yuyu Hakusho Live-Action coming on Netflix on December 14, 2023' },
+      { property: 'og:image', content: 'http://spurbank-images.s3.ap-southeast-2.amazonaws.com/yuyu-hakusho.png' }
     ]);
 
-    this.setTitle(this.blogTitle);
   }
 
   private addTags(tags: { property: string, content: string }[]): void {
     tags.forEach(tag => this.meta.addTag({ property: tag.property, content: tag.content }));
-  }
-
-  private setTitle(newTitle: string): void {
-    this.title.setTitle(newTitle);
   }
 }
 
